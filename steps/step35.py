842db2f3fc83db14a31c32e7d7a8c4299bfa471e
rumjie <diwoon95@gmail.com>
2023-05-22 22:38:45 +0900

    step35 add

---
+++ steps/step35.py
@@ -0,0 +1,30 @@
+# 상위 폴더 참조
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import plot_dot_graph
+import dezero.functions as F
+
+
+x = Variable(np.array(1.0))
+y = F.tanh(x)
+x.name = "x"
+y.name = "y"
+y.backward(create_graph=True)
+
+iters = 0
+
+for i in range(iters):
+    gx = x.grad
+    x.cleargrad()
+    gx.backward(create_graph=True)
+
+
+# 그래프 그리기
+gx = x.grad
+gx.name = "gx" + str(iters + 1)
+plot_dot_graph(gx, verbose=False, to_file="tanh.png")
